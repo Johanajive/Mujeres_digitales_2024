187a674810ff91f7a303e4944e20c439842d9d0e
Johanajive <johanajime233@gmail.com>
2025-09-03 19:43:25 -0500

    first commit

---
+++ par_impar.js
@@ -0,0 +1,13 @@
+const rl = require('readline').createInterface({ input: process.stdin, output: process.stdout });
+rl.question('Escribe un número: ', (n) => {
+    const numero = Number(n);
+
+    if (isNaN(numero)) {
+        console.log("Entrada ivalida, no es un número");
+    } else if (numero % 2 === 0) {
+        console.log("Tú número es par")
+    } else {
+        console.log("Tú número es impar")
+    }
+    rl.close();
+});
